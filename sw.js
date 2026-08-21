cfd5e161b5a038009ec2384af7638047d67c14aa
aurelien59fr-a11y <aurelien59fr@gmail.com>
2026-08-21 10:31:42 +0200

    Bump cache v21

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - Bump CACHE_VERSION à chaque déploiement pour forcer la mise à jour
 ============================================================ */
 
-const CACHE_VERSION = 'bradford-v20';
+const CACHE_VERSION = 'bradford-v21';
 const APP_SHELL = [
   './',
   './index.html',
