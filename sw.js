bffde88d25d226c9b21e80563b27c228879f8f77
aurelien59fr-a11y <aurelien59fr@gmail.com>
2026-08-22 13:16:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - Bump CACHE_VERSION à chaque déploiement pour forcer la mise à jour
 ============================================================ */
 
-const CACHE_VERSION = 'bradford-v33';
+const CACHE_VERSION = 'bradford-v34';
 const APP_SHELL = [
   './',
   './index.html',
