63f870050a1e1f5343817651880e171f33687739
aurelien59fr-a11y <aurelien59fr@gmail.com>
2026-08-25 17:13:50 +0200

    Cache bust v47 / bradford-v50 (operateur Inpak deduit sur fenetre du defaut)

--- sw.js
+++ sw.js
@@ -8,11 +8,11 @@
    - Bump CACHE_VERSION à chaque déploiement pour forcer la mise à jour
 ============================================================ */
 
-const CACHE_VERSION = 'bradford-v49';
+const CACHE_VERSION = 'bradford-v50';
 const APP_SHELL = [
   './',
   './index.html',
-  './js/app.js?v=46',
+  './js/app.js?v=47',
   './manifest.json',
   './icons/icon-180.png',
   './icons/icon-192.png',
