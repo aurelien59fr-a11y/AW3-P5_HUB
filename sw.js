3bf22e49e1a5ea6f9a26dce624e6d728b7d7960a
aurelien59fr-a11y <aurelien59fr@gmail.com>
2026-08-30 11:44:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - Bump CACHE_VERSION à chaque déploiement pour forcer la mise à jour
 ============================================================ */
 
-const CACHE_VERSION = 'bradford-v39';
+const CACHE_VERSION = 'bradford-v40';
 const APP_SHELL = [
   './',
   './index.html',
