1c62c072fccaf0ed999a7e3d7411871e101941bb
aurelien59fr-a11y <aurelien59fr@gmail.com>
2026-09-23 18:28:46 +0200

    sw: cache v90 (correctifs Logbook)

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - Bump CACHE_VERSION a chaque deploiement pour forcer la mise a jour
 ============================================================ */
 
-const CACHE_VERSION = 'bradford-v89';
+const CACHE_VERSION = 'bradford-v90';
 const APP_SHELL = [
      './',
      './index.html',
@@ -25,7 +25,7 @@ const APP_SHELL = [
      './js/metier/ncp.js?v=2',
      './js/vues/ncp.js?v=1',
      './js/metier/logbook.js?v=1',
-     './js/vues/logbook.js?v=1',
+     './js/vues/logbook.js?v=2',
      './js/metier/bulk.js?v=1',
      './js/vues/bulk.js?v=2',
      './js/vues/planning.js?v=6',
@@ -37,7 +37,7 @@ const APP_SHELL = [
      './js/vues/absences.js?v=3',
      './js/vues/ov-resume.js?v=6',
      './js/app.js?v=86',
-     './css/styles.css?v=87',
+     './css/styles.css?v=88',
      './manifest.json',
      './icons/icon-180.png',
      './icons/icon-192.png',
