d0f3aee6e6b52d48adb96334b5a401230a2eb82d
aurelien59fr-a11y <aurelien59fr@gmail.com>
2026-09-25 09:29:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - Bump CACHE_VERSION a chaque deploiement pour forcer la mise a jour
 ============================================================ */
 
-const CACHE_VERSION = 'bradford-v92';
+const CACHE_VERSION = 'bradford-v93';
 const APP_SHELL = [
      './',
      './index.html',
@@ -22,11 +22,11 @@ const APP_SHELL = [
      './js/imports/grafana.js?v=1',
      './js/imports/protime.js?v=2',
      './js/imports/ncp.js?v=2',
-     './js/imports/sharepoint.js?v=1',
+     './js/imports/sharepoint.js?v=2',
      './js/metier/ncp.js?v=2',
      './js/vues/ncp.js?v=1',
-     './js/metier/logbook.js?v=3',
-     './js/vues/logbook.js?v=4',
+     './js/metier/logbook.js?v=4',
+     './js/vues/logbook.js?v=5',
      './js/metier/bulk.js?v=1',
      './js/vues/bulk.js?v=2',
      './js/vues/planning.js?v=6',
@@ -38,7 +38,7 @@ const APP_SHELL = [
      './js/vues/absences.js?v=3',
      './js/vues/ov-resume.js?v=6',
      './js/app.js?v=86',
-     './css/styles.css?v=90',
+     './css/styles.css?v=91',
      './manifest.json',
      './icons/icon-180.png',
      './icons/icon-192.png',
